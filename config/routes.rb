38f0452f7e6f8aefdc8e1a404a2d1775bf8cc942
Dominic Brown and Sowmya <sowmya@>
2017-07-11 16:45:24 -0700

    Add home page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "products#index"
+  root to: "home#index"
+  resources :home, only: [:index]
 
   resources :products do
     resources :comments
