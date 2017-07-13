b0f915806a40ef5152a1054dbbad2d394b149f11
Dominic Brown and sowmya dinavahi <sowmya@>
2017-07-12 18:48:57 -0700

    update cart bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,10 @@ class ProductsController < ApplicationController
     @products = Product.all
     @order_item = current_order.order_items.new
     @order_items = current_order.order_items
+    # respond_to do |format|
+    #   format.html { redirect_to products_url }
+    #   format.js
+    # end
   end
 
   def show
